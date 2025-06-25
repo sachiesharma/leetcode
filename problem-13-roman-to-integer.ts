bafaa489fc3fc09039c5caab2c30be8023b275e8
Sachie Sharma <sachie@gmail.com>
2025-06-25 17:43:18 -0400

    Complete Roman to Integer problem

--- problem-13-roman-to-integer.ts
+++ problem-13-roman-to-integer.ts
@@ -8,10 +8,27 @@ function romanToInt(s: string): number {
     D: 500,
     M: 1000,
   };
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    let currentValue = romanMap[s[i]];
+    let nextValue = romanMap[s[i + 1]];
+
+    if (currentValue < nextValue) {
+      total -= currentValue;
+    } else {
+      total += currentValue;
+    }
+  }
+  return total;
 }
+
+console.log(romanToInt("III"));
 
 // Notes
 // Map out the roman numerals and their values
 // there are seven different symbols: I:, V, X, L, C, D, M
-// they are written largest to smallest: with six exceptions
+// they are written largest to smallest, left to right
+// Exception where subtraction is used: six exceptions
+
+// If large value is written before smaller value, add
 // If a smaller value appears before larger value, subtract
